4cac2def0d9ccfed01549d6d3c0ef1c60029ef01
miftakhulfalh <65790130+miftakhulfalh@users.noreply.github.com>
2025-04-23 10:07:13 +0700

    Update bot.js

--- api/bot.js
+++ api/bot.js
@@ -3,7 +3,7 @@ const { Telegraf } = require('telegraf');
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
 // Handler untuk /start
-bot.start((ctx) => {
+bot.command('start', (ctx) => {
   ctx.reply('Halo! Selamat datang di bot Telegram yang berjalan di Vercel 🚀');
 });
 
@@ -11,12 +11,12 @@ module.exports = async (req, res) => {
   if (req.method === 'POST') {
     try {
       await bot.handleUpdate(req.body);
-      res.status(200).send('OK');
+      return res.status(200).send('OK');
     } catch (err) {
       console.error('Error handling update', err);
-      res.status(500).send('Error');
+      return res.status(500).send('Error');
     }
   } else {
-    res.status(200).send('Bot endpoint aktif ✅');
+    return res.status(200).send('Bot endpoint aktif ✅');
   }
 };
